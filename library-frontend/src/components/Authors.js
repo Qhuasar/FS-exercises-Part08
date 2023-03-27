7fa35ac7e2663a6b9d64abee763362683159ad28
Pedro Matos <pedroddematos@gmail.com>
2023-03-27 21:08:16 +0100

    ex_8.12

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,67 @@
+import { useMutation, useQuery } from "@apollo/client"
+import { useState } from "react"
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
+
+const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+  const [name, setName] = useState("")
+  const [birthDate, setBirthDate] = useState("")
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
+  if (!props.show) {
+    return null
+  }
+  if (authors.loading) {
+    return <div>Loading...</div>
+  }
+
+  const handleUpdate = (e) => {
+    e.preventDefault()
+    editAuthor({ variables: { name, setBornTo: parseInt(birthDate, 10) } })
+    setName("")
+    setBirthDate("")
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <form onSubmit={(e) => handleUpdate(e)}>
+        <h1>Update Birth Date</h1>
+        <label>name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <p />
+        <label>date</label>
+        <input
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+          type="number"
+        ></input>
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
